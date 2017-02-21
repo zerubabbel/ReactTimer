29aa354675cb29bdc80ca7dcadbdf8dd2ac0fe22
zerubabbel <achiappetta90@gmail.com>
2017-02-20 16:26:01 -0800

    clock tests

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,8 +1,14 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = React.createClass({
   render: function () {
-    return <p>Countdown.jsx</p>;
+    return
+      (
+      <div>
+        <Clock totalSeconds={129}/>
+      </div>
+    );
   }
 });
 
