b975128beb9149417507e65b845d76023bd8beca
zerubabbel <achiappetta90@gmail.com>
2017-02-25 22:49:51 -0800

    finalize tests and add page title

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -35,6 +35,29 @@ describe('Countdown', () => {
         }, 3001)
       });
 
+      it('should pause countdown on paused status', () => {
+        var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+        countdown.handleSetCountdown(3);
+        countdown.handleStatusChange('paused');
+
+        setTimeout(() => {
+          expect(countdown.state.count).toBe(3);
+          expect(countdown.state.countdownStatus).toBe('paused');
+          done();
+        }, 1001);
+      });
+
+      it('should reset count on stopped', () => {
+        var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+        countdown.handleSetCountdown(3);
+        countdown.handleStatusChange('stopped');
+
+        setTimeout(() => {
+          expect(countdown.state.count).toBe(0);
+          expect(countdown.state.countdownStatus).toBe('stopped');
+          done();
+        }, 1001);
+      });
 
   });
 });
